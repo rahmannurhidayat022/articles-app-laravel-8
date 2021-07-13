6153edeb4c9120e60ca681c208f2c5285823ab98
Rahman Nurhidayat <rahmannurhidayat022@gmail.com>
2021-07-13 16:18:00 +0700

    add sidabar to layouts.app

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,71 +1,44 @@
 @extends('layouts.app')
 
+@section('title-page')
+Articles    
+@endsection
+
 @section('content')
 @if ($data != null)
-<div class="container-fluid">
-    <div class="row">
-      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
-        <div class="position-sticky pt-3">
-          <ul class="nav flex-column">
-            <li class="nav-item text-white">
-                <h4>Dashboard</h4>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" href="{{ url('/home') }}">
-                <span><i class="bi bi-card-checklist"></i></span>
-                Articles
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" href="{{ url('/articles/new') }}">
-                <span><i class="bi bi-file-earmark-plus"></i></span>
-                Create Articles
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-  
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-white">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Articles</h1>
-        </div>
-        <div class="table-responsive">
-          <table class="table table-striped table-sm">
-            <thead>
-              <tr>
+<div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
                 <th scope="col">No</th>
                 <th scope="col">Author</th>
                 <th scope="col">Title</th>
                 <th scope="col">Writen</th>
                 <th scope="col">Publish</th>
                 <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-                @foreach ($data as $item)
-              <tr>
-                    <td>#</td>
-                    <td>{{ $item->author }}</td>
-                    <td>{{ $item->title }}</td>
-                    <td>{{ $item->created_at }}</td>
-                    @if ($item->published_at == null)
-                    <td class="text-info">DRAF</td>
-                    @else
-                    <td>{{ $item->published_at }}</td>
-                    @endif
-                    <td>
-                        <a class="btn btn-info btn-block" href="{{ url('/articles/detail/'.$item->id) }}">DETAIL</a>
-                    </td>
-                </tr>
-                @endforeach
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </main>
-    </div>
-  </div>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $item)
+            <tr>
+                <td>#</td>
+                <td>{{ $item->author }}</td>
+                <td>{{ $item->title }}</td>
+                <td>{{ $item->created_at }}</td>
+                @if ($item->published_at == null)
+                <td class="text-info">DRAF</td>
+                @else
+                <td>{{ $item->published_at }}</td>
+                @endif
+                <td>
+                    <a class="btn btn-info btn-block" href="{{ url('/articles/detail/'.$item->id) }}">DETAIL</a>
+                </td>
+            </tr>
+            @endforeach
+            </tr>
+        </tbody>
+    </table>
+</div>
 @else
 <div class="alert alert-primary" role="alert">
     <h4 class="alert-heading">Cache sedang dibersihkan!</h4>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,39 @@
 
         <main class="py-4">
             <div class="container">
-                @yield('content')
+                <div class="container-fluid">
+                    <div class="row">
+                      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+                        <div class="position-sticky pt-3">
+                          <ul class="nav flex-column">
+                            <li class="nav-item text-white">
+                                <h4>Dashboard</h4>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link text-white" href="{{ url('/home') }}">
+                                <span><i class="bi bi-card-checklist"></i></span>
+                                Articles
+                              </a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link text-white" href="{{ url('/articles/new') }}">
+                                <span><i class="bi bi-file-earmark-plus"></i></span>
+                                Create Articles
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
+                      </nav>
+                  
+                      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-white">
+                        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                          <h1 class="h2">@yield('title-page')</h1>
+                        </div>
+                        @yield('content')
+                        </div>
+                      </main>
+                    </div>
+                  </div>
             </div>
         </main>
     </div>
